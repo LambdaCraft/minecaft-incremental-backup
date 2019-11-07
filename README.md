fedeebdf4640e961c3ebb8562eb4949a3bac8cf4
unsupervised <unsupervised@tutanota.com>
2019-11-06 18:18:55 -0800

    Use duplicity and tmux instead of file copy and screen

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Minecraft Backup
-Backup script for Linux servers running a Minecraft server in a GNU Screen
+Differential backup script for Linux servers running a Minecraft server in tmux using duplicity
 
 ### Disclaimer
 Backups are essential to the integrity of your Minecraft world. You should automate regular backups and **check that your backups work**. While this script has been used in production for several years, it is up to you to make sure that your backups work and that you have a reliable backup policy. 
@@ -8,16 +8,15 @@ Please refer to the LICENSE (MIT License) for the full legal disclaimer.
 
 ## Features
 - Create backups of your world folder
-- Manage deletion of old backups
-  - "thin" - keep last 24 hourly, last 30 daily, and use remaining space for monthly backups
-  - "sequential" - delete oldest backup
-- Choose your own compression algorithm (tested with: `gzip`, `xz`, `zstd`)
+- Configurable number of full backups to keep
 - Able to print backup status and info to the Minecraft chat
 
 ## Requirements
-- Linux computer (tested on Ubuntu)
-- GNU Screen (running your Minecraft server)
-- Minecraft server (tested with Vanilla 1.10.2 only)
+- Linux computer (tested on CentOS)
+- tmux (running your Minecraft server)
+- rsync (remote backup sync)
+- duplictiy (differential backup binary)
+- Minecraft server (tested with Vanilla 1.14.4)
 
 ## Installation
 1. Download the script: `$ wget https://raw.githubusercontent.com/nicolaschan/minecraft-backup/master/backup.sh`
@@ -26,49 +25,32 @@ Please refer to the LICENSE (MIT License) for the full legal disclaimer.
 
 Command line options:
 ```text
--a    Compression algorithm (default: gzip)
 -c    Enable chat messages
--d    Delete method: thin (default), sequential, none
--e    Compression file extension, exclude leading "." (default: gz)
 -f    Output file name (default is the timestamp)
 -h    Shows this help text
 -i    Input directory (path to world folder)
--l    Compression level (default: 3)
--m    Maximum backups to keep, use -1 for unlimited (default: 128)
 -o    Output directory
 -p    Prefix that shows in Minecraft chat (default: Backup)
 -q    Suppress warnings
--s    Minecraft server screen name
+-s    Minecraft server tmux session name
 -v    Verbose mode
 ```
 
 Example usage of command line options:
 ```bash
-./backup.sh -c -i /home/server/minecraft-server/world -o /mnt/external-storage/minecraft-backups -s minecraft
+./backup.sh -c -i /home/server/minecraft-server/world -o /mnt/external-storage/minecraft-backups -r username@remote_host:destination_directory -s minecraft
 ```
-This will use show chat messages (`-c`) in the screen called "minecraft" and save a backup of `/home/server/minecraft-server/world` into `/mnt/external-storage/minecraft-backups` using the default thinning delete policy for old backups.
+This will use show chat messages (`-c`) in the screen called "minecraft" and save a backup of `/home/server/minecraft-server/world` into `/mnt/external-storage/minecraft-backups` and rsync it into `username@remote_host:destination_directory`.
+
+By default it will do a full backup if the last full backup is older than 2 weeks, and keep 2 full backup cycles.
 
 4. Create a cron job to automatically backup:
     - Edit the crontab: `$ crontab -e`
     - Example for hourly backups: `00 * * * * /path/to/backup.sh`
   
 ## Retrieving Backups
-Always test your backups! Backups are in the `tar` format and compressed depending on the option you choose. To restore, first decompress if necessary and then extract using tar. You may be able to do this in one command if `tar` supports your compression option, as is the case with `gzip`:
-
-Example:
-```bash
-mkdir restored-world
-cd restored-world
-tar -xzvf /path/to/backups/2019-04-09_02-15-01.tar.gz
-```
-
-Then you can move your restored world (`restored-world` in this case) to your Minecraft server folder and rename it (usually called `world`) so the Minecraft server uses it.
+Follow duplicity docs to learn how to restore your server
 
 ## Help
-- Make sure the compression algorithm you specify is installed on your system. (zstd is not installed by default)
-- Make sure your compression algorithm is in the crontab's PATH
 - Make sure cron has permissions for all the files involved and access to the Minecraft server's GNU Screen
-- It's surprising how much space backups can take--make sure you have enough empty space
-- `SERVER_DIRECTORY` should be the server directory, not the `world` directory
 - Do not put trailing `/` in the `SERVER_DIRECTORY` or `BACKUP_DIRECTORY`
-- If "thin" delete method is behaving weirdly, try emptying your backup directory or switch to "sequential"
--- backup.sh
+++ backup.sh
@@ -1,25 +1,29 @@
 #!/bin/bash
 
 # Minecraft server automatic backup management script
-# by Nicolas Chan
+# by Nicolas Chan & Lambdacraft
 # MIT License
 #
-# For Minecraft servers running in a GNU screen.
+# For Minecraft servers running in a tmux.
 # For most convenience, run automatically with cron.
 
 # Default Configuration 
-SCREEN_NAME="" # Name of the GNU Screen your Minecraft server is running in
-SERVER_WORLD="" # Server world directory
-BACKUP_DIRECTORY="" # Directory to save backups in
-MAX_BACKUPS=128 # -1 indicates unlimited
-DELETE_METHOD="thin" # Choices: thin, sequential, none; sequential: delete oldest; thin: keep last 24 hourly, last 30 daily, and monthly (use with 1 hr cron interval)
-COMPRESSION_ALGORITHM="gzip" # Leave empty for no compression
-COMPRESSION_FILE_EXTENSION=".gz" # Leave empty for no compression; Precede with a . (for example: ".gz")
-COMPRESSION_LEVEL=3 # Passed to the compression algorithm
 ENABLE_CHAT_MESSAGES=false # Tell players in Minecraft chat about backup status
 PREFIX="Backup" # Shows in the chat message
 DEBUG=false # Enable debug messages
 SUPPRESS_WARNINGS=false # Suppress warnings
+BK_KEEP_FULL="2"  # How many full+inc cycle to keep
+# Following option takes an interval, which is a number followed by one of the characters s, m, h, D, W, M, or Y (indicating seconds, minutes, hours, days, weeks, months, or years respectively)
+BK_FULL_FREQ="2W" # create a new full backup every...
+
+# Required Configuration
+SESSION_PANE_NAME="" # Name of the tmux session and pane your Minecraft server is running in, ex. minecraft.0
+DATA_DIR="" # Fully qualified directory of data to backup, be careful not to put a trailing slash, ex: /root/tobackup and not /root/tobackup/
+LOCAL_BACKUP_DIRECTORY="" # Directory to save backups in
+REMOTE_BACKUP_DIRECTORY="" # URL to remote backup folder, ex: username@remote_host:destination_directory
+
+# Advanced
+PREFIX_CMD="nice -n18 ionice -c 3"
 
 # Other Variables (do not modify)
 DATE_FORMAT="%F_%H-%M-%S"
@@ -27,35 +31,28 @@ TIMESTAMP=$(date +$DATE_FORMAT)
 
 while getopts 'a:cd:e:f:hi:l:m:o:p:qs:v' FLAG; do
   case $FLAG in
-    a) COMPRESSION_ALGORITHM=$OPTARG ;;
     c) ENABLE_CHAT_MESSAGES=true ;;
-    d) DELETE_METHOD=$OPTARG ;;
-    e) COMPRESSION_FILE_EXTENSION=".$OPTARG" ;;
+    # e) COMPRESSION_FILE_EXTENSION=".$OPTARG" ;;
     f) TIMESTAMP=$OPTARG ;;
-    h) echo "Minecraft Backup (by Nicolas Chan)"
-       echo "-a    Compression algorithm (default: gzip)"
+    h) echo "Minecraft Backup"
        echo "-c    Enable chat messages"
-       echo "-d    Delete method: thin (default), sequential, none"
-       echo "-e    Compression file extension, exclude leading \".\" (default: gz)"
        echo "-f    Output file name (default is the timestamp)"
        echo "-h    Shows this help text"
        echo "-i    Input directory (path to world folder)"
-       echo "-l    Compression level (default: 3)"
-       echo "-m    Maximum backups to keep, use -1 for unlimited (default: 128)"
        echo "-o    Output directory"
        echo "-p    Prefix that shows in Minecraft chat (default: Backup)"
        echo "-q    Suppress warnings"
-       echo "-s    Minecraft server screen name"
+       echo "-r    Remote directory"
+       echo "-s    Minecraft server tmux session name"
        echo "-v    Verbose mode"
        exit 0
        ;;
-    i) SERVER_WORLD=$OPTARG ;;
-    l) COMPRESSION_LEVEL=$OPTARG ;;
-    m) MAX_BACKUPS=$OPTARG ;;
-    o) BACKUP_DIRECTORY=$OPTARG ;;
+    i) DATA_DIR=$OPTARG ;;
+    o) LOCAL_BACKUP_DIRECTORY=$OPTARG ;;
     p) PREFIX=$OPTARG ;;
     q) SUPPRESS_WARNINGS=true ;;
-    s) SCREEN_NAME=$OPTARG ;;
+    r) REMOTE_BACKUP_DIRECTORY=$OPTARG ;;
+    s) SESSION_PANE_NAME=$OPTARG ;;
     v) DEBUG=true ;;
   esac
 done
@@ -67,29 +64,38 @@ log-warning () {
   echo -e "\033[0;33mWARNING:\033[0m $*"
 }
 
+# Check for missing dependencies
+FATAL=false
+if ! hash rsync || ! hash duplicity || ! hash tmux; then
+  log-fatal "Missing required dependency"
+  FATAL=true
+fi
+
 # Check for missing encouraged arguments
-if ! $SUPPRESS_WARNINGS; then
-  if [[ $SCREEN_NAME == "" ]]; then
-    log-warning "Minecraft screen name not specified (use -s)"
-  fi
+if [[ $SESSION_PANE_NAME == "" ]]; then
+  log-fatal "Minecraft tmux session name not specified (use -s)"
+  FATAL=true
 fi
 # Check for required arguments
-MISSING_CONFIGURATION=false
-if [[ $SERVER_WORLD == "" ]]; then
-  log-fatal "Server world not specified (use -i)"
-  MISSING_CONFIGURATION=true
-fi
-if [[ $BACKUP_DIRECTORY == "" ]]; then
-  log-fatal "Backup directory not specified (use -o)"
-  MISSING_CONFIGURATION=true
+
+if [[ $DATA_DIR == "" ]]; then
+  log-fatal "Server data directory not specified (use -i)"
+  FATAL=true
 fi
 
-if $MISSING_CONFIGURATION; then
+if [[ $LOCAL_BACKUP_DIRECTORY == "" ]]; then
+  log-fatal "Local backup directory not specified (use -o)"
+  FATAL=true
+fi
+
+if [[ $REMOTE_BACKUP_DIRECTORY == "" ]]; then
+  log-fatal "Local backup directory not specified (use -o)"
+  FATAL=true
+fi
+
+if $FATAL; then
   exit 0
 fi
-
-ARCHIVE_FILE_NAME=$TIMESTAMP.tar$COMPRESSION_FILE_EXTENSION
-ARCHIVE_PATH=$BACKUP_DIRECTORY/$ARCHIVE_FILE_NAME
 
 # Minecraft server screen interface functions
 message-players () {
@@ -99,8 +105,8 @@ message-players () {
 }
 execute-command () {
   local COMMAND=$1
-  if [[ $SCREEN_NAME != "" ]]; then
-    screen -S $SCREEN_NAME -p 0 -X stuff "$COMMAND$(printf \\r)"
+  if [[ $SESSION_PANE_NAME != "" ]]; then
+    tmux send-keys -t $SESSION_PANE_NAME "$COMMAND" ENTER
   fi
 }
 message-players-error () {
@@ -125,150 +131,40 @@ message-players-color () {
   fi
 }
 
-# Notify players of start
-message-players "Starting backup..." "$ARCHIVE_FILE_NAME"
+# Warn players
+message-players "Backup starting in 5 minutes"
+sleep 4m
 
-# Parse file timestamp to one readable by "date" 
-parse-file-timestamp () {
-  local DATE_STRING=$(echo $1 | awk -F_ '{gsub(/-/,":",$2); print $1" "$2}')
-  echo $DATE_STRING
-}
-
-# Delete a backup
-delete-backup () {
-  local BACKUP=$1
-  rm $BACKUP_DIRECTORY/$BACKUP
-  message-players "Deleted old backup" "$BACKUP"
-}
-
-# Sequential delete method
-delete-sequentially () {
-  local BACKUPS=($(ls $BACKUP_DIRECTORY))
-  while [[ $MAX_BACKUPS -ge 0 && ${#BACKUPS[@]} -gt $MAX_BACKUPS ]]; do
-    delete-backup ${BACKUPS[0]}
-    BACKUPS=($(ls $BACKUP_DIRECTORY))
-  done
-}
-
-# Functions to sort backups into correct categories based on timestamps
-is-hourly-backup () {
-  local TIMESTAMP=$*
-  local MINUTE=$(date -d "$TIMESTAMP" +%M)
-  return $MINUTE
-}
-is-daily-backup () {
-  local TIMESTAMP=$*
-  local HOUR=$(date -d "$TIMESTAMP" +%H)
-  return $HOUR
-}
-is-weekly-backup () {
-  local TIMESTAMP=$*
-  local DAY=$(date -d "$TIMESTAMP" +%u)
-  return $((DAY - 1))
-}
-
-# Helper function to sum an array
-array-sum () {
-  SUM=0
-  for NUMBER in $*; do
-    (( SUM += NUMBER ))
-  done
-  echo $SUM
-}
-
-# Thinning delete method
-delete-thinning () {
-  # sub-hourly, hourly, daily, weekly is everything else
-  local BLOCK_SIZES=(16 24 30)
-  # First block is unconditional
-  # The next blocks will only accept files whose names cause these functions to return true (0)
-  local BLOCK_FUNCTIONS=("is-hourly-backup" "is-daily-backup" "is-weekly-backup")
-
-  # Warn if $MAX_BACKUPS does not have enough room for all the blocks
-  TOTAL_BLOCK_SIZE=$(array-sum ${BLOCK_SIZES[@]})
-  if [[ $TOTAL_BLOCK_SIZE -gt $MAX_BACKUPS ]]; then
-    if ! $SUPPRESS_WARNINGS; then
-      log-warning "MAX_BACKUPS ($MAX_BACKUPS) is smaller than TOTAL_BLOCK_SIZE ($TOTAL_BLOCK_SIZE)"
-    fi
-  fi
-
-  local CURRENT_INDEX=0
-  local BACKUPS=($(ls -r $BACKUP_DIRECTORY)) # List newest first
-
-  for BLOCK_INDEX in ${!BLOCK_SIZES[@]}; do
-    local BLOCK_SIZE=${BLOCK_SIZES[BLOCK_INDEX]}
-    local BLOCK_FUNCTION=${BLOCK_FUNCTIONS[BLOCK_INDEX]}
-    local OLDEST_BACKUP_IN_BLOCK_INDEX=$((BLOCK_SIZE + CURRENT_INDEX)) # Not an off-by-one error because a new backup was already saved 
-    local OLDEST_BACKUP_IN_BLOCK=${BACKUPS[OLDEST_BACKUP_IN_BLOCK_INDEX]}
-
-    if [[ $OLDEST_BACKUP_IN_BLOCK == "" ]]; then
-      break
-    fi
-
-    local OLDEST_BACKUP_TIMESTAMP=$(parse-file-timestamp ${OLDEST_BACKUP_IN_BLOCK:0:19})
-    local BLOCK_COMMAND="$BLOCK_FUNCTION $OLDEST_BACKUP_TIMESTAMP"
-
-    if $BLOCK_COMMAND; then
-      # Oldest backup in this block satisfies the condition for placement in the next block
-      if $DEBUG; then
-        echo "$OLDEST_BACKUP_IN_BLOCK promoted to next block" 
-      fi
-    else
-      # Oldest backup in this block does not satisfy the condition for placement in next block
-      delete-backup $OLDEST_BACKUP_IN_BLOCK
-      break
-    fi
-
-    ((CURRENT_INDEX += BLOCK_SIZE))
-  done
-
-  delete-sequentially
-}
+# Save the world
+execute-command "save-all"
+# wait for save to finish
+sleep 1m
 
 # Disable world autosaving
 execute-command "save-off"
 
+# Notify players of start
+message-players "Starting backup..." "$ARCHIVE_FILE_NAME"
+
 # Backup world
+LOCAL_DUP_DIR="file://$LOCAL_BACKUP_DIRECTORY"
 START_TIME=$(date +"%s")
-case $COMPRESSION_ALGORITHM in
-  "") # No compression
-    tar -cf $ARCHIVE_PATH -C $SERVER_WORLD .
-    ;;
-  *) # With compression
-    tar -cf - -C $SERVER_WORLD . | $COMPRESSION_ALGORITHM -cv -$COMPRESSION_LEVEL - > $ARCHIVE_PATH 2>> /dev/null
-    ;;
-esac
+
+${PREFIX_CMD} duplicity --no-encryption --allow-source-mismatch --full-if-older-than $BK_FULL_FREQ $DATA_DIR $LOCAL_DUP_DIR
+${PREFIX_CMD} duplicity --allow-source-mismatch remove-all-but-n-full $BK_KEEP_FULL --force $LOCAL_DUP_DIR
+
+if ! rsync -avh -e "ssh -p22" "$LOCAL_BACKUP_DIRECTORY/" $REMOTE_BACKUP_DIRECTORY --delete ; then
+  message-players-error "Failed to transfer backup to remote server"
+fi
+
 sync
+
 END_TIME=$(date +"%s")
 
 # Enable world autosaving
 execute-command "save-on"
 
-# Save the world
-execute-command "save-all"
-
-# Delete old backups
-delete-old-backups () {
-  case $DELETE_METHOD in
-    "sequential") delete-sequentially
-      ;;
-    "thin") delete-thinning
-      ;;
-  esac
-}
-
 # Notify players of completion
-WORLD_SIZE_BYTES=$(du -b --max-depth=0 $SERVER_WORLD | awk '{print $1}')
-ARCHIVE_SIZE_BYTES=$(du -b $ARCHIVE_PATH | awk '{print $1}')
-COMPRESSION_PERCENT=$(($ARCHIVE_SIZE_BYTES * 100 / $WORLD_SIZE_BYTES))
-ARCHIVE_SIZE=$(du -h $ARCHIVE_PATH | awk '{print $1}')
-BACKUP_DIRECTORY_SIZE=$(du -h --max-depth=0 $BACKUP_DIRECTORY | awk '{print $1}')
 TIME_DELTA=$((END_TIME - START_TIME))
 
-# Check that archive size is not null and at least 1024 KB
-if [[ "$ARCHIVE_SIZE" != "" && "$ARCHIVE_SIZE_BYTES" -gt 8 ]]; then
-  message-players-success "Backup complete!" "$TIME_DELTA s, $ARCHIVE_SIZE/$BACKUP_DIRECTORY_SIZE, $COMPRESSION_PERCENT%"
-  delete-old-backups
-else
-  message-players-error "Backup was not saved!" "Please notify an administrator"
-fi
+message-players-success "Backup complete!" "$TIME_DELTA s"
